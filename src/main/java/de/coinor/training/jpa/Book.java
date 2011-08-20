82b9b03ec850f8966f115c1065350719596e87aa
Gregor Tudan <G.Tudan@gmx.de>
2011-08-20 17:54:22 +0200

    starting basic structure

---
+++ src/main/java/de/coinor/training/jpa/Book.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package de.coinor.training.jpa;
+
+import java.util.Date;
+
+/**
+ * @author gtudan
+ *
+ */
+public class Book {
+	private String title;
+	private String author;
+	private int pages;
+	private double price;
+	private Date published;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getPages() {
+		return pages;
+	}
+	
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Date getPublished() {
+		return published;
+	}
+
+	public void setPublished(Date published) {
+		this.published = published;
+	}
+}
