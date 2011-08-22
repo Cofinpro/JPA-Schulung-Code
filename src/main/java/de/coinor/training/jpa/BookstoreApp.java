164685f1b0139e844b6d6c05e68c088cb3b341c8
Gregor Tudan <G.Tudan@gmx.de>
2011-08-22 07:32:21 +0200

    added hibernate and log4j configuration

---
+++ src/main/java/de/coinor/training/jpa/BookstoreApp.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.coinor.training.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * @author gtudan
+ *
+ */
+public class BookstoreApp {
+
+	EntityManagerFactory emFactory;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new BookstoreApp();
+
+	}
+	
+	public BookstoreApp() {
+		emFactory = Persistence.createEntityManagerFactory("Bookstore");
+	}
+
+}
